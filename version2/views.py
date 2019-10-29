9a9433d7f4cc38c72e87590b390d7e95fddd6c1b
Danny <saelid2517@gmail.com>
2019-10-28 19:55:30 -0700

    added left/right randomization and a midway switch

--- version2/views.py
+++ version2/views.py
@@ -4,19 +4,34 @@ from version2.extraction import *
 from django.shortcuts import redirect
 from version2.models import *
 import random
-# Create your views here.
-first_algorithm = "0g"
-second_algorithm = "rp"
+
 num_search_results = 5
-# gets relevant data from snippet.pickle file
-first_snippets = extractFromFile(first_algorithm + ".txt", num_search_results)
-second_snippets = extractFromFile(second_algorithm + ".txt", num_search_results)
+# maps algorithm names to lists of snippets
+# algorithms to be displayed on the left and right, respectively
+left_alg = "0g"
+right_alg = "rp"
+# algorithms to be displayed on left and right after 10 turns
+round_two_l = "1g"
+round_two_r = "05g"
+
+alg_to_snippets = {
+    left_alg: extractFromFile(left_alg + ".txt", num_search_results),
+    right_alg: extractFromFile(right_alg + ".txt", num_search_results),
+    round_two_l: extractFromFile(round_two_l + ".txt", num_search_results),
+    round_two_r: extractFromFile(round_two_r + ".txt", num_search_results)
+}
+# algorithms to be displayed on the left and right, respectively
+left_alg = "0g"
+right_alg = "rp"
+
+# whether or not to swap the left and right algorithms on a given turn
+swap = [False, True, True, False, True, True, True, False, False, False, False, False, True, True, False, False, True, False, True, True]
 unseen_qids = []
 num_qids_seen = 0
 curr_qid = 0
 respondent = None
 
-for qid in first_snippets:
+for qid in alg_to_snippets[left_alg]:
     unseen_qids.append(qid)
 
 def getRandomQid():
@@ -35,11 +50,13 @@ def consent(request):
 def demographics(request):
     if 'age' in request.GET:
         global respondent
+        """ Uncomment for database action
         respondent = Respondent(
             age=request.GET['age'],
             gender=request.GET['gender'],
             education=request.GET['education'])
         respondent.save()
+        """
         return redirect('version2-instructions')
     else:
         return render(request, 'version2/demographics.html')
@@ -49,45 +66,68 @@ def instructions(request):
 
 def home(request):
     global curr_qid
-    curr_qid = getRandomQid()
-
+    curr_qid = getNextQid()
+    print("Left: " + left_alg)
+    print("Right: " + right_alg)
     context = {
-        'first_snippets': first_snippets[curr_qid],
-        'second_snippets': second_snippets[curr_qid],
-        'query_name': first_snippets[curr_qid][0][0]
+        'left_snippets': alg_to_snippets[left_alg][curr_qid],
+        'right_snippets': alg_to_snippets[right_alg][curr_qid],
+        'query_name': alg_to_snippets[right_alg][curr_qid][0][0]
     }
     return render(request, 'version2/home.html', context);
 
+num_responses = 0
 def update(request):
     global num_qids_seen
     global curr_qid
+    global left_alg
+    global right_alg
+    global num_responses
+    
     num_qids_seen += 1
-    if (num_qids_seen < 20):
+    if (num_qids_seen <= 20):
         # send data to server
         # only if the user selected an option
         if 'radio' in request.GET:
             choice = ''
             not_choice = ''
-            if request.GET['radio'] == 'first':
-                choice = first_algorithm
-                not_choice = second_algorithm
+            if request.GET['radio'] == 'left':
+                choice = left_alg
+                not_choice = right_alg
             else:
-                choice = second_algorithm
-                not_choice = first_algorithm
-                
+                choice = right_alg
+                not_choice = left_alg
+            print("User chose: " + choice)
+            num_responses += 1
+            """ Uncomment for database action
             response = Response(respondent=respondent,
                                 query=Query.objects.filter(query_id=curr_qid)[0],
                                 chosen_alg=Algorithm.objects.filter(name=choice)[0],
                                 unchosen_alg=Algorithm.objects.filter(name=not_choice)[0],
                                 time_elapsed=int(request.GET['time_elapsed']))
             response.save()
+            """
+        if num_qids_seen == 10:
+            print("Switching to second and third")
+            left_alg = round_two_l
+            right_alg = round_two_r
+            
+        if swap[num_qids_seen - 1]:
+            print("Swapping")
+            temp = left_alg
+            left_alg = right_alg
+            right_alg = temp
             
         # redirect to home
-        return redirect('version2-home')
+        if num_qids_seen < 20:
+            return redirect('version2-home')
+        else:
+            return redirect('version2-thanks')
     else:
-        # should I reset unseen_qids and num_qids_seen here?
         return redirect('version2-thanks')
-    
+
 def thanks(request):
+    global num_responses
+    print(num_responses)
     return render(request, 'version2/thanks.html')
     
